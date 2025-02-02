d3293421e1a89a353590281004922a7529197d00
SoKnight <mcliyanage2018@gmail.com>
2025-02-03 01:44:56 +0500

    Remove useless variable

--- buildSrc/src/main/kotlin/fabric-platform.gradle.kts
+++ buildSrc/src/main/kotlin/fabric-platform.gradle.kts
@@ -1,11 +1,6 @@
-import elfeatures.gradle.model.Mod
-
 plugins {
     id("fabric-loom")
 }
-
-// load Mod info
-val mod: Mod = rootProject.extra["mod"] as Mod
 
 // configure resources processing
 tasks.processResources {
